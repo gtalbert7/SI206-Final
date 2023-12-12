364f057e1f2e4e0423a09bee4164a89228ebf9cb
Griffin Talbert <griffintalbert@Joes-Air.localdomain>
2023-12-11 21:17:00 -0500

    load_data update for genres

--- load_data.py
+++ load_data.py
@@ -4,13 +4,18 @@ from bs4 import BeautifulSoup
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
+import sqlite3
+import requests
+from bs4 import BeautifulSoup
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
 def load_data():
     response = requests.get('https://top40weekly.com/top-100-artists-of-the-10s/')
     webpage_content = response.text
     soup = BeautifulSoup(webpage_content, 'html.parser')
     client_id = '84c99acc62714269b6ff371dc4e7dc97'
     client_secret = 'b28f081661b64fb592a3c63994614f65'
-    #spotipy_redirect_url='https://localhost:8888/callback'
     auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
     sp = spotipy.Spotify(auth_manager=auth_manager)
 
@@ -25,7 +30,8 @@ def load_data():
     CREATE TABLE IF NOT EXISTS artists (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL UNIQUE,
-        spotify_id TEXT NOT NULL UNIQUE
+        spotify_id TEXT NOT NULL UNIQUE,
+        genre TEXT
     )
     ''')
 
@@ -36,14 +42,16 @@ def load_data():
     end_index = start_index + batch_size
     next_batch = artist_names[start_index:end_index]
 
-    #i = 0
     for artist in next_batch:
-    #    i += 1
         result = sp.search(q='artist:' + artist, type='artist')
         items = result['artists']['items']
-        spotify_id = items[0]['id'] if items else None
-        cursor.execute('INSERT OR IGNORE INTO artists (name, spotify_id) VALUES (?, ?)', (artist, spotify_id))
-    #print(i)
+        if items:
+            spotify_id = items[0]['id']
+            genres = items[0]['genres']
+            genre = genres[0] if genres else "Unknown"
+            cursor.execute('INSERT OR IGNORE INTO artists (name, spotify_id, genre) VALUES (?, ?, ?)', (artist, spotify_id, genre))
+        else:
+            cursor.execute('INSERT OR IGNORE INTO artists (name) VALUES (?)', (artist,))
     conn.commit()
 
     cursor.execute('SELECT * FROM artists')
